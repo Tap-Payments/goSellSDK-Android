b6156e03e59b2bfcff877ae07d377f67f87103c9
Philonenko Andrey <a.philonenko@tap.company>
2018-05-09 18:10:06 +0300

    added layout and activity of card address screen

---
+++ library/src/main/java/company/tap/gosellapi/internal/activities/GoSellCardAddressActivity.java
@@ -0,0 +1,16 @@
+package company.tap.gosellapi.internal.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import company.tap.gosellapi.R;
+
+public class GoSellCardAddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gosellapi_activity_card_address);
+    }
+}
+
