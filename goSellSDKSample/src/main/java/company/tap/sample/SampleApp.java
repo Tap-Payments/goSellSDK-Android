28c3a68e3b904b705e927da234dccf13f37fcabf
GoltsevEugene <goltsev.e@gmail.com>
2018-05-24 15:14:11 +0300

    Initial Fabric build, installed crashlytics, updated app name.

--- goSellSDKSample/src/main/java/company/tap/sample/SampleApp.java
+++ goSellSDKSample/src/main/java/company/tap/sample/SampleApp.java
@@ -3,12 +3,15 @@ package company.tap.sample;
 import android.app.Application;
 
 import company.tap.gosellapi.GoSellSDK;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class SampleApp extends Application{
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         GoSellSDK.init(this, "sk_test_0spETzD5rvIjOoM8mwqJA27i");
     }
