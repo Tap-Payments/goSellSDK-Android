31539cb8665cccb59a06378e6b7cb611c2fb0116
Philonenko Andrey <a.philonenko@tap.company>
2018-07-23 17:09:26 +0300

    GSL-61

--- library/src/main/java/company/tap/gosellapi/internal/viewholders/CardCredentialsViewHolder.java
+++ library/src/main/java/company/tap/gosellapi/internal/viewholders/CardCredentialsViewHolder.java
@@ -1,10 +1,15 @@
 package company.tap.gosellapi.internal.viewholders;
 
+import android.hardware.camera2.CameraDevice;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SwitchCompat;
 import android.text.Editable;
+import android.text.InputFilter;
+import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +19,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 import company.tap.gosellapi.R;
@@ -32,6 +38,7 @@ import company.tap.tapcardvalidator_android.DefinedCardBrand;
 public class CardCredentialsViewHolder
         extends PaymentOptionsBaseViewHolder<ArrayList<PaymentOption>, CardCredentialsViewHolder, CardCredentialsViewModel> {
     private final static int BIN_NUMBER_LENGTH = 6;
+    private final static int NAME_ON_CARD_MAX_LENGTH = 26;
 
     ImageButton cardScannerButton;
     private View addressOnCardLayout;
@@ -54,7 +61,6 @@ public class CardCredentialsViewHolder
         cardNumberField = itemView.findViewById(R.id.cardNumberField);
 
 
-
         // Card number field
         cardNumberField.addTextChangedListener(new TextWatcher() {
 
@@ -71,21 +77,20 @@ public class CardCredentialsViewHolder
 
                 int[] spacings;
                 if (cardBrand != CardBrand.americanExpress) {
-                    spacings = new int[] {4, 8, 12};
-                }
-                else {
-                    spacings = new int[] {3, 9};
+                    spacings = new int[]{4, 8, 12};
+                } else {
+                    spacings = new int[]{3, 9};
                 }
 
                 String text = s.toString();
                 text = text.replace(" ", "");
                 SpannableStringBuilder cardNumber = new SpannableStringBuilder(text);
 
-                for(int i = spacings.length - 1; i >= 0; i--) {
+                for (int i = spacings.length - 1; i >= 0; i--) {
 
                     int space = spacings[i];
 
-                    if(space < text.length()) {
+                    if (space < text.length()) {
                         cardNumber.insert(space, " ");
                     }
                 }
@@ -95,25 +100,24 @@ public class CardCredentialsViewHolder
 
                 int selectionIndex = start + count;
 
-                for(int i = 0; i < spacings.length; i++) {
+                for (int i = 0; i < spacings.length; i++) {
 
                     int space = spacings[i];
 
                     int incrementedSpace = space + i;
 
-                    if(start <= incrementedSpace && incrementedSpace < start + count) {
+                    if (start <= incrementedSpace && incrementedSpace < start + count) {
                         selectionIndex++;
                     }
 
-                    if(start - 1 == incrementedSpace && count == 0) {
+                    if (start - 1 == incrementedSpace && count == 0) {
                         selectionIndex--;
                     }
                 }
 
                 try {
                     cardNumberField.setSelection(selectionIndex);
-                }
-                catch (Exception e) {
+                } catch (Exception e) {
                     cardNumberField.setSelection(cardNumber.length());
                 }
 
@@ -128,6 +132,32 @@ public class CardCredentialsViewHolder
 
         // CVVField
         CVVField = itemView.findViewById(R.id.CVVField);
+
+        CVVField.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    CVVField.setText("");
+                }
+            }
+        });
+
+        CVVField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         // Date field
         expirationDateField = itemView.findViewById(R.id.expirationDateField);
@@ -145,6 +175,44 @@ public class CardCredentialsViewHolder
 
         // Name on card
         nameOnCardField = itemView.findViewById(R.id.nameOnCardField);
+
+        nameOnCardField.setFilters(new InputFilter[] {new InputFilter.AllCaps(), new InputFilter.LengthFilter(NAME_ON_CARD_MAX_LENGTH),
+        new InputFilter() {
+
+            @Override
+            public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+
+                boolean keepOriginal = true;
+
+                StringBuilder sb = new StringBuilder(end - start);
+
+                for (int i = start; i < end; i++) {
+                    char c = source.charAt(i);
+
+                    if (isCharAllowed(c)) // put your condition here
+                        sb.append(c);
+                    else
+                        keepOriginal = false;
+                }
+
+                if (keepOriginal)
+                    return null;
+                else {
+                    if (source instanceof Spanned) {
+                        SpannableString sp = new SpannableString(sb);
+                        TextUtils.copySpansFrom((Spanned) source, start, sb.length(), null, sp, 0);
+                        return sp;
+                    } else {
+                        return sb;
+                    }
+                }
+            }
+
+            private boolean isCharAllowed(char c) {
+                return Character.isLetterOrDigit(c) || Character.isSpaceChar(c);
+            }
+
+        } });
     }
 
     @Override
@@ -229,7 +297,6 @@ public class CardCredentialsViewHolder
 
     private DefinedCardBrand validateCardNumber(String cardNumber) {
 
-        // 415254 - test bin number
         cardNumber = cardNumber.replace(" ", "");
 
         DefinedCardBrand brand = CardValidator.validate(cardNumber);
@@ -256,5 +323,10 @@ public class CardCredentialsViewHolder
         }
 
         return brand;
+    }
+
+    private void validateCardCVV(String CVVNumber) {
+
+
     }
 }
