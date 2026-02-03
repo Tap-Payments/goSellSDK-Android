3c71570f1f6aa2dd6a589ff4fbcee7afc21fa3ac
Ahlaam K <a.kazi@tap.company>
2026-02-03 12:31:10 +0300

    remove keys

--- goSellSDKSample/src/main/java/company/tap/sample/activity/MainActivity.java
+++ goSellSDKSample/src/main/java/company/tap/sample/activity/MainActivity.java
@@ -149,7 +149,7 @@ public class MainActivity extends AppCompatActivity implements SessionDelegate {
      * Configure SDK with your Secret API key and App Bundle name registered with tap company.
      */
     private void configureApp(){
-       GoSellSDK.init(this, "sk_test_kovrMB0mupFJXfNZWx6Etg5y","company.tap.goSellSDKExample");  // to be replaced by merchant
+       GoSellSDK.init(this, "sk_XXXXXXXXXXXXXXXXXXXXXXXX","company.tap.goSellSDKExample");  // to be replaced by merchant
         GoSellSDK.setLocale("en");//  language to be set by merchant
     }
     /**
