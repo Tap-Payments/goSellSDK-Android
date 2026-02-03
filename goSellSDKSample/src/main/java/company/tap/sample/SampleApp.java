ce1491b77b2b12aa2b332c3160965ffb3d854114
Ahlaam K <a.kazi@tap.company>
2026-02-03 12:34:18 +0300

    remove keys

--- goSellSDKSample/src/main/java/company/tap/sample/SampleApp.java
+++ goSellSDKSample/src/main/java/company/tap/sample/SampleApp.java
@@ -13,7 +13,7 @@ public class SampleApp extends Application{
     public void onCreate() {
         super.onCreate();
 
-          GoSellSDK.init(this, "sk_test_kovrMB0mupFJXfNZWx6Etg5y","company.tap.goSellSDKExample");                 // to be replaced by merchant
+          GoSellSDK.init(this, "sk_XXXXXXXXXXXXXXXXXXXXXXXX","company.tap.goSellSDKExample");                 // to be replaced by merchant
           GoSellSDK.setLocale("en");
     }
 }
